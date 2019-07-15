9084ad07c53177a78e41c411118294817136318e
level42ca <jeffrey.borg@gmail.com>
2019-07-15 12:01:11 -0400

    Donation/Credits/ minor adjustments

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,30 @@
-const button = document.querySelector('#dropdown-button');
+const ddbutton = document.querySelector('#dropdown-button');
 const subMenu = document.querySelector('#dropdown-menu');
 const subMenuItem = document.querySelectorAll('#dropdown-menu li');
 const searchButton = document.querySelector('#search-button');
 const searchBox = document.querySelector('#search-box');
 
-window.addEventListener('resize', () => {    
-	if (window.innerWidth <= 750) {	
-		searchButton.innerHTML = 'Search <i class="fal fa-search">';
-	} else {
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 750) {
+    searchButton.innerHTML = 'Search <i class="fal fa-search">';
+  } else {
     searchButton.innerHTML = '<i class="fal fa-search">';
   }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  button.addEventListener('click', () => subMenu.style.display = 'block');
-
+  ddbutton.addEventListener('click', () => subMenu.style.display = 'block');
+  console.log("tRUE")
   subMenuItem.forEach(item => {
     item.addEventListener('click', () => {
-      let { value } = item.attributes.value;
-      button.innerHTML = `${item.innerText} <i class="fal fa-${value}">`;
+      let {
+        value
+      } = item.attributes.value;
+      ddbutton.innerHTML = `${item.innerText} <i class="fal fa-${value}">`;
       subMenu.style.display = 'none';
     });
   });
-  
+
   searchButton.addEventListener('click', search);
   document.addEventListener('keyup', e => {
     if (e.which == 13) search();
@@ -32,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function search() {
-	let type = button.innerText;
+  let type = ddbutton.innerText;
   let query = searchBox.value;
   let commonToAll = `
     -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml)
@@ -45,7 +47,7 @@ function search() {
     'Audio': '(ac3|flac|m4a|mp3|ogg|wav|wma)',
     'Ebook': '(CBZ|CBR|CHM|DOC|DOCX|EPUB|MOBI|ODT|PDF|RTF|txt)',
     'Pictures': '(bmp|gif|jpg|png|psd|tif|tiff)',
-    'Software/Games': '(apk|exe|iso|rar|tar|zip)',
+    'Software/Games': '(apk|exe|ipa|iso|rar|tar|zip)',
     'Compressed': '(apk|exe|iso|rar|tar|zip|7z)'
   }
 
